0496f3b3e9a6190ec8ac57f3396cc3e86f8deff2
Elkin Gabriel <latinohablantes@gmail.com>
2024-03-15 00:07:52 -0500

    Ejercicio de procedimientos almacenados

--- Procedimientos-Almacenados/Ejercicio-proc-almacenados.sql
+++ Procedimientos-Almacenados/Ejercicio-proc-almacenados.sql
@@ -1,6 +1,24 @@
+create database Hospital;
+
+use Hospital;
+
+
 -- SOLUCION DE EJERCICIOS
 
 -- PROCEDIMIENTOS ALMACENADOS
+
+-- 1. Construya el procedimiento almacenado que saque todos los empleados que se dieron de
+-- alta entre una determinada fecha inicial y fecha final y que pertenecen a un determinado
+-- departamento.
+
+select *
+from Emp
+WHERE Fecha_Alt BETWEEN "1980-01-01" AND "1984-01-01";
+
+DELIMITER //
+
+CREATE PROCEDURE empEntreFecha(IN fecha_ini date, IN fecha_fin date)
+
 
 SELECT * FROM Emp;
 
